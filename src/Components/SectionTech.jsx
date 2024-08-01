c0afa42e79dc706c3d818b4f098b30add8de7ebc
Surya prakash <suryaharsh279@gmail.com>
2024-08-01 13:18:16 +0530

    ..

--- src/Components/SectionTech.jsx
+++ src/Components/SectionTech.jsx
@@ -96,6 +96,12 @@ const Tech = [
         name: "Cloudinary",
         link: "https://th.bing.com/th?id=OIP.82kz9qEta_iEtdQsp6UuZAHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
         content: "Cloud-based image and video management solution"
+    },
+    {
+        name:"NextJS",
+        link:"https://th.bing.com/th/id/OIP.l7Sf8CQ7XDFbiN0JRc7vOAHaHa?w=193&h=193&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        content:"Currently Working on Next JS learn modrn routing and rendering"
+
     }
 ];
 
